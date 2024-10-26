1810fd8fd8c5c0ebaa44b9430ff7e204c902b992
who? <164739638+Floppat@users.noreply.github.com>
2024-10-26 14:02:12 +0300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -5,8 +5,6 @@ import discord
 from embeds import eq2,eq3,eq4,eq5
 
 # global warming buttons-------------------------------------------------------------------
-#создать словарь и добавлять туда 'dict[id] = []' 'dict[id].append(True)'
-#await ctx.send('ohoho rules from evil daddy', embeds=[embed, embed1])
 class bq1(discord.ui.View):
     def __init__(self, *, timeout = 180, user: int):
         super().__init__(timeout=timeout)
@@ -25,10 +23,6 @@ class bq1(discord.ui.View):
     async def bq13(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.edit_message(content="Неправильный ответ. правильный ответ был: B", embed=eq2, view=bq2(user=self.user))
     
-    @discord.ui.button(label='закончить', style=discord.ButtonStyle.danger)
-    async def end1(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.user.quizresult(interaction=interaction, cancel = True)
-
 
 
 class bq2(discord.ui.View):
@@ -48,10 +42,6 @@ class bq2(discord.ui.View):
     @discord.ui.button(label='C', style=discord.ButtonStyle.secondary)
     async def bq23(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.edit_message(content="Неправильный ответ. правильный ответ был: А", embed=eq3, view=bq3(user=self.user))
-
-    @discord.ui.button(label='закончить', style=discord.ButtonStyle.danger)
-    async def end2(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.user.quizresult(interaction=interaction, cancel = True)
 
 
 
@@ -73,10 +63,6 @@ class bq3(discord.ui.View):
     async def bq33(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.edit_message(content="Неправильный ответ. правильный ответ  был: B", embed=eq4, view=bq4(user=self.user))
 
-    @discord.ui.button(label='закончить', style=discord.ButtonStyle.danger)
-    async def end3(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.user.quizresult(interaction=interaction, cancel = True)
-
 
 
 class bq4(discord.ui.View):
@@ -97,10 +83,6 @@ class bq4(discord.ui.View):
         await self.user.right_answer()
         await interaction.response.edit_message(content="Правильный ответ!", embed=eq5, view=bq5(user=self.user))
 
-    @discord.ui.button(label='закончить', style=discord.ButtonStyle.danger)
-    async def end4(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.user.quizresult(interaction=interaction, cancel = True)
-
 
 
 class bq5(discord.ui.View):
@@ -111,12 +93,10 @@ class bq5(discord.ui.View):
     @discord.ui.button(label='A', style=discord.ButtonStyle.secondary)
     async def bq51(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.user.right_answer()
-        await self.user.quizresult(interaction=interaction, cancel=False)
-
+        await self.user.quizresult(interaction=interaction)
     @discord.ui.button(label='B', style=discord.ButtonStyle.secondary)
     async def bq52(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.user.quizresult(interaction=interaction, cancel=False)
-
+        await self.user.quizresult(interaction=interaction)
     @discord.ui.button(label='C', style=discord.ButtonStyle.secondary)
     async def bq53(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.user.quizresult(interaction=interaction, cancel=False)
+        await self.user.quizresult(interaction=interaction)
