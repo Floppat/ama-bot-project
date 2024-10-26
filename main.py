8bbd45806dba4451de3f433302f54b2d0c52c7ce
who? <164739638+Floppat@users.noreply.github.com>
2024-10-26 15:54:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,64 +24,55 @@ async def cmd(interaction: discord.Interaction) -> None:
 
 # mini-game commands------------------------------------------------------------------------
 @bot.command('cmd_game')
-async def cmd_game(ctx: commands.Context):
-    await mini_game_cmd(ctx)
+async def cmd_game(ctx: commands.Context) -> None:
+    await mini_game_cmd(ctx=ctx)
+
 @bot.tree.command(name='prehistory', description='ура сюжет')
 async def prehistory(interaction: discord.Interaction) -> None:
-    await mini_game_prehistory(interaction)
+    await mini_game_prehistory(interaction=interaction)
 
 @bot.tree.command(name='guide', description='Отображает справку по игре')
 async def guide(interaction: discord.Interaction) -> None:
-    await mini_game_guide(interaction)
-
-@bot.tree.command(name='register', description='Позволяет зарегистрироваться в игре')
-async def register(interaction: discord.Interaction, nickname: str) -> None:
-    await mini_game_register(interaction, nickname)
+    await mini_game_guide(interaction=interaction)
 
 @bot.tree.command(name='train', description='Потренируйте питомца!')
 async def train(interaction: discord.Interaction) -> None:
-    await mini_game_train(interaction)
+    await mini_game_train(interaction=interaction)
 
 @bot.tree.command(name='feed', description='Покормите питомца!')
 async def feed(interaction: discord.Interaction) -> None:
-    await mini_game_feed(interaction)
+    await mini_game_feed(interaction=interaction)
 
 @bot.tree.command(name='attack', description=' Настало время драки!')
 async def attack(interaction: discord.Interaction) -> None:
-    await mini_game_attack(interaction)
+    await mini_game_attack(interaction=interaction)
 
 @bot.tree.command(name='sleep', description='У-устал')
 async def sleep(interaction: discord.Interaction) -> None:
-    await mini_game_sleep(interaction)
+    await mini_game_sleep(interaction=interaction)
 
 @bot.tree.command(name='shop', description='Напишите ? или help если не знаете ассортимент')
 async def shop(interaction: discord.Interaction, item: str) -> None:
-    await mini_game_shop(interaction, item)
-
-@bot.tree.command(name='goofyahh', description='супер важная секретная команда')
-async def goofyahh(interaction: discord.Interaction) -> None:
-    await mini_game_goofyahh(interaction)
-
+    await mini_game_shop(interaction=interaction, item=item)
 # global warming commands-------------------------------------------------------------------
 @bot.command('cmd_warming')
 async def cmd_warming(ctx: commands.Context) -> None:
-    await global_warming_cmd(ctx)
+    await global_warming_cmd(ctx=ctx)
 @bot.command('about')
 async def about(ctx: commands.Context) -> None:
-    await global_warming_about(ctx)
+    await global_warming_about(ctx=ctx)
 
 @bot.command('reasons')
 async def reasons(ctx: commands.Context) -> None:
-    await global_warming_reasons(ctx)
+    await global_warming_reasons(ctx=ctx)
 
 @bot.command('how_help')
 async def how_help(ctx: commands.Context) -> None:
-    await global_warming_how_help(ctx)
+    await global_warming_how_help(ctx=ctx)
 
 @bot.tree.command(name='quiz', description='небольшая викторина о глобальном потеплении')
-async def quiz(ctx: commands.Context):
-    await global_warming_quiz(ctx)
-
+async def quiz(interaction: discord.Interaction) -> None:
+    await global_warming_quiz(interaction=interaction)
 
 # bot run-----------------------------------------------------------------------------------
 bot.run('')
